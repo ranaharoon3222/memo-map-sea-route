4d58cc10387cb4aca74a0b2d09a1810417878a2e
ranaharoon3222 <ranaharoon3222@gmail.com>
2025-04-17 12:32:05 +0500

    fixed

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ const { getNearbyData } = require('./locationQue');
 const NodeCache = require('node-cache');
 const oneYearInSeconds = 365 * 24 * 60 * 60;
 
-// 1 month check period = 30 days × 24 hours × 60 minutes × 60 seconds
-const oneMonthInSeconds = 30 * 24 * 60 * 60;
+const twentyDaysInSeconds = 20 * 24 * 60 * 60;
 
 const routeCache = new NodeCache({
-  stdTTL: oneYearInSeconds, // items expire in 1 year
-  checkperiod: oneMonthInSeconds, // cleanup runs every 1 month
+  stdTTL: oneYearInSeconds,
+  checkperiod: twentyDaysInSeconds,
 });
 
 // Number of workers based on CPU cores
